1f7a497cdb1c0fa9a08a15a5738fc0116a93699e
xuhuigithub <1498472791@qq.com>
2017-10-03 23:12:28 +0800

    增加了与用户组的多对一关系（外键，多台主机对一个用户组）

--- cmdb/hostinfo/models.py
+++ cmdb/hostinfo/models.py
@@ -1,6 +1,8 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models
+from userinfo.models import Group
 
 # Create your models here.
 class Hostinfo(models.Model):
@@ -8,4 +10,5 @@ class Hostinfo(models.Model):
   ip_addr = models.CharField(max_length=20)
   ipmi_addr = models.CharField(max_length=20)
   password = models.CharField(max_length=64)
-  
+  owner_group = models.ForeignKey(Group)
+  #默认的 django.db.models.ForeignKey.on_delete set to CASCADE 也就是在这个数据模型中，当你的组被删除后，你所有的属于这个组的主机也同样会被删除
