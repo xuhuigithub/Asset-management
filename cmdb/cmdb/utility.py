3dbc222472a39e25f3d17262362b2876c891211a
xuhuigithub <1498472791@qq.com>
2017-10-05 22:24:35 +0800

    添加了common_api函数

--- cmdb/cmdb/utility.py
+++ cmdb/cmdb/utility.py
@@ -1,6 +1,23 @@
+from django.http import HttpResponse
+import json
+
+common_api_result = {'status':True,'msg':None,'error':None}
 
 def Filtered_var(var):
   if var != None and len(var) > 0:
     return var
   else:
-    return None 
+    return None
+
+def common_api(func,request,api_result):
+  result = api_result
+  try:
+    if request.method == "POST":
+      func(request)
+    else:
+      pass
+  except Exception as e:
+    result['error'] = str(e)
+  finally:
+    result = json.dumps(result, indent=4)
+    return HttpResponse(result, content_type="application/json")
