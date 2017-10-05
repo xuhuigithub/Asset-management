16ce6c8e49a6f284f742cb8163eba9666713e313
xuhuigithub <1498472791@qq.com>
2017-10-05 22:34:02 +0800

    修改了用户表的数据结构

--- cmdb/userinfo/models.py
+++ cmdb/userinfo/models.py
@@ -2,11 +2,17 @@ from django.db import models
 
 # Create your models here.
 from django.db import models
-
-class Userinfo(models.Model):
-  email = models.EmailField()
-  password = models.CharField(max_length=64)
-
-
 class Group(models.Model):
   name = models.CharField(max_length=30)
+
+class Userinfo(models.Model):
+  # REQUIRED_FIELDS = ('email','password',)
+  # USERNAME_FIELD = 'username'
+  username = models.CharField(max_length=32,unique=True)
+  email = models.EmailField(unique=True)
+  password = models.CharField(max_length=64)
+  is_actived = models.BooleanField(default=True)
+  group = models.ManyToManyField('Group')
+
+
+
