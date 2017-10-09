0b024b8337f1e7812a3ea11a78fbc82f66071b33
xuhuigithub <1498472791@qq.com>
2017-10-09 23:56:33 +0800

    增加了静态模板的存放位置

--- cmdb/cmdb/settings.py
+++ cmdb/cmdb/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'cmdb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR+"/templates",],
+        'DIRS': [BASE_DIR+"/templates",os.path.join(BASE_DIR, '/templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
